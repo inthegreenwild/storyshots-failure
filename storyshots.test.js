919572ee3d6217fb58cb81d8ad08c158b2980968
dcairns <dylan.cairns@morningstar.com>
2022-10-31 11:48:16 -0500

    test

--- storyshots.test.js
+++ storyshots.test.js
@@ -1,5 +1,17 @@
-const initStoryshots = require("@storybook/addon-storyshots").default;
+import initStoryshots from "@storybook/addon-storyshots";
+import { imageSnapshot } from "@storybook/addon-storyshots-puppeteer";
+
+const getMatchOptions = ({}) => {
+  return {
+    failureThreshold: 0,
+  };
+};
 
 initStoryshots({
   framework: "vue3",
+  suite: "avr",
+  framework: "vue3",
+  test: imageSnapshot({
+    getMatchOptions,
+  }),
 });
